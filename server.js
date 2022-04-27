0560489059022eecbda9d53c8a7da5f92bf2d18e
TimK76 <haadelphos2001@gmail.com>
2022-04-27 19:46:37 -0400

    add a candidate api route successful

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Import Express and MySQL
 const express = require("express");
 const mysql = require("mysql2");
+const inputCheck = require('./utils/inputCheck');
 
 // Adds PORT designation
 const PORT = process.env.PORT || 3001;
@@ -67,7 +68,7 @@ app.get("/api/candidate/:id", (req, res) => {
 });
 
 // Delete a candidate
-app.delete('/api/candidate/:id', (req, res) => {
+app.delete("/api/candidate/:id", (req, res) => {
   const sql = `DELETE FROM candidates WHERE id = ?`;
   const params = [req.params.id];
 
@@ -76,22 +77,39 @@ app.delete('/api/candidate/:id', (req, res) => {
       res.statusMessage(400).json({ error: res.message });
     } else if (!result.affectedRows) {
       res.json({
-        message: 'Candidate not found'
+        message: "Candidate not found",
       });
     } else {
       res.json({
-        message: 'deleted',
+        message: "deleted",
         changes: result.affectedRows,
-        id: req.params.id
+        id: req.params.id,
       });
     }
   });
 });
 
-// // Query: Create a Candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?, ?, ?, ?)`;
-// const params = [1, "Ronald", "Firbank", 1];
+// Query: Create a Candidate
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(body, "first_name", "last_name", "industry_connected");
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
 
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
+});
 // db.query(sql, params, (err, result) => {
 //   if (err) {
 //     console.log(err);
